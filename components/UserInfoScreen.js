7741e0adb39eea6b244a745f2344c03ed08d16b7
TRILE\Tri Le <lehanhtri287@gmail.com>
2018-05-25 22:16:57 +0700

    create user info screen

---
+++ components/UserInfoScreen.js
@@ -0,0 +1,24 @@
+import React, {Component} from 'react';
+
+import {
+    View
+}
+    from 'react-native';
+
+export default class UserInfoScreen extends Component {
+    render() {
+        return (
+            <View style={styleShow.container}>
+
+
+            </View>
+        )
+    }
+}
+
+var styleShow = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#FFB6C1'
+    }
+})
